579d57f8412a20289d9e66f7a80150eb38bc68e0
Jessie366 <jessieyin366@gmail.com>
2025-05-08 18:54:01 +1000

    fix repair.php

--- repair.php
+++ repair.php
@@ -18,6 +18,7 @@ if (!$conn) {
 
 // Read request body
 $data = json_decode(file_get_contents('php://input'), true);
+file_put_contents('php://stderr', "RECEIVED: " . json_encode($data) . "\n");
 $name = $data['name'] ?? '';
 $unit = $data['unit'] ?? '';
 $desc = $data['repairDescription'] ?? '';
