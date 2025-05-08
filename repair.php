fcf95647625b8ea5f7785907b59abcabd77afb36
Jessie366 <jessieyin366@gmail.com>
2025-05-08 21:29:33 +1000

    idk

--- repair.php
+++ repair.php
@@ -4,7 +4,7 @@ header('Access-Control-Allow-Methods: POST, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Content-Type: application/json');
 
-// ✅ 预检请求
+// ✅ 预检请求jsbasb xjhas 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200);
     exit;
